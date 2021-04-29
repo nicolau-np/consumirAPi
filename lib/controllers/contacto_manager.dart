1bf7a8bc50dfdc53f69adea78e8fa90a525fc5a2
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-29 22:04:22 +0100

    update

--- lib/controllers/contacto_manager.dart
+++ lib/controllers/contacto_manager.dart
@@ -1,8 +1,16 @@
-import 'package:http/http.dart';
-class ContactoManager{
+import 'dart:convert';
 
+import 'package:consumirapi/models/contacto_model.dart';
+import 'package:http/http.dart';
+
+class ContactoManager {
   final String url = "http://127.0.0.1:8000/api/contactos";
 
-
-
+  Future<List<ContactoModel>> getContactos() async {
+    Response res = await get(url);
+    List<dynamic> body = jsonDecode(res.body);
+    List<ContactoModel> contactos =
+        body.map((dynamic json) => ContactoModel.fromJson(json)).toList();
+    return contactos;
+  }
 }
