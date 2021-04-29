1a42a37b254046167570f3e125b1f66e40e3a8f5
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-29 19:09:16 +0100

    update request

--- bin/sync_async.dart
+++ bin/sync_async.dart
@@ -1,2 +1,12 @@
 import 'package:http/http.dart' as http;
 
+void main() {
+  fetch();
+}
+
+Future fetch() async {
+  var url = "http://127.0.0.1:8000/api/contactos";
+  var response = await http.get(url);
+
+  print(response.body);
+}
