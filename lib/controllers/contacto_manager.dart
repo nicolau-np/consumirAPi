9bf08bb3944b4fac3afb3f6998d654e49869198b
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-29 22:49:05 +0100

    trabalhando no controller

--- lib/controllers/contacto_manager.dart
+++ lib/controllers/contacto_manager.dart
@@ -1,23 +1,46 @@
 import 'dart:convert';
 
+import 'package:consumirapi/helpers/api_request.dart';
+import 'package:consumirapi/helpers/const.dart';
 import 'package:consumirapi/models/contacto_model.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 
-class ContactoManager {
-  final String url = "http://127.0.0.1:8000/api/contactos";
-
-  List<ContactoModel> lista = [];
+class ContactoManager extends ChangeNotifier {
+  ContactoModel contactoModel = ContactoModel();
   ContactoManager() {
     getContactos();
   }
-  
-  Future<List<ContactoModel>> getContactos() async {
-    Response res = await get(url);
-    List<dynamic> body = jsonDecode(res.body);
-    List<ContactoModel> contactos =
-        body.map((dynamic json) => ContactoModel.fromJson(json)).toList();
-    
-    return contactos;
+
+  bool _isLoading = false;
+
+  bool get loading => _isLoading;
+  set loading(bool value) {
+    _isLoading = value;
+    notifyListeners();
   }
 
+  Future<ApiResponse<ContactoModel>> getContactos() async {
+    try {
+      var url = '$BASE_URL/contactos/';
+
+      var response = await http.get(url);
+
+      Map mapRensponse = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        contactoModel = ContactoModel.fromJson(mapRensponse);
+
+        notifyListeners();
+        return ApiResponse.ok(contactoModel);
+      }
+      notifyListeners();
+      return ApiResponse.error(mapRensponse["message"]);
+    } catch (e) {
+      print(
+        "Erro no login $e",
+      );
+      return ApiResponse.error("Impossivel fazer login");
+    }
+  }
 }
