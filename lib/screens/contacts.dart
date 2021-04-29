5ed1f7e764dfc96b8de784ae56dfac51e081feb2
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-29 18:52:43 +0100

    ordenando a tela de contactos

--- lib/screens/contacts.dart
+++ lib/screens/contacts.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+
 class ContactosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contactos"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('hello world'),
+          ),
+        ],
+      ),
     );
   }
 }
