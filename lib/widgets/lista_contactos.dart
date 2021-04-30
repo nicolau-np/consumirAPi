932822e00a82e63904671c06da65b640d812a524
Nicolau NP <mr1Normaliii@gmail.com>
2021-04-30 10:43:00 +0100

    trabalhando na visualizacao dos contactos

--- lib/widgets/lista_contactos.dart
+++ lib/widgets/lista_contactos.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:consumirapi/models/contacto_model.dart';
+import 'package:consumirapi/services/api_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
@@ -10,20 +11,32 @@ class ListaContactos extends StatefulWidget {
 }
 
 class _ListaContactosState extends State<ListaContactos> {
+  Future<ContactoModel> _contactoMOdel;
+
   @override
   Widget build(BuildContext context) {
     @override
     void initState() {
+      _contactoMOdel = Api_Manager().getContactos();
       super.initState();
     }
 
     return Container(
-      child: ListView.builder(itemBuilder: (context, index) {
-        return Container(
-          height: 100,
-          color: Colors.red,
-        );
-      }),
+      child: FutureBuilder(
+        future: _contactoMOdel,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(itemBuilder: (context, index) {
+              return Container(
+                height: 100,
+                color: Colors.red,
+              );
+            });
+          }else{
+            return CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
